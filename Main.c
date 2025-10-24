9e24338ac18b08edd7f1bd7c08f3f5df85d13d1f
Mateus <mateusdesouzaroseno@gmail.com>
2025-10-24 16:22:13 -0300

    Convert Main.java to Main.c with threaded execution

---
+++ Main.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+//THREADS ENCADEADAS
+
+//THREAD 1
+void* t1(void* arg) {
+    
+    sleep(1);
+    
+    printf("thread1 iniciada!\n");
+    
+    sleep(1);
+    
+    printf("thread1 finalizada!\n");
+    
+    pthread_exit(NULL);
+}
+
+//THREAD 2
+void* t2(void* arg) {
+    
+    sleep(1);
+    
+    printf("thread2 iniciada!\n");
+    
+    pthread_t thread1;
+    
+    int res1 = pthread_create(&thread1, NULL, t1, NULL);
+
+    if (res1 != 0) {
+        perror("Erro ao criar a thread");
+        exit(1);
+    }
+    
+    sleep(1);
+
+    pthread_join(thread1, NULL);
+    
+    sleep(1);
+    
+    printf("thread2 finalizada!\n");
+    
+    pthread_exit(NULL);
+}
+
+//THREAD 3
+void* t3(void* arg) {
+    
+    sleep(1);
+    
+    printf("thread3 iniciada!\n");
+    
+    pthread_t thread2;
+    
+    int res2 = pthread_create(&thread2, NULL, t2, NULL);
+
+    if (res2 != 0) {
+        perror("Erro ao criar a thread");
+        exit(1);
+    }
+
+    pthread_join(thread2, NULL);
+    
+    sleep(1);
+    
+    printf("thread3 finalizada!\n");
+    
+    pthread_exit(NULL);
+}
+
+//THREAD 4
+void* t4(void* arg) {
+    
+    sleep(1);
+    
+    printf("thread4 iniciada!\n");
+    
+    pthread_t thread3;
+
+     int res3 = pthread_create(&thread3, NULL, t3, NULL);
+    
+    if (res3 != 0) {
+        perror("Erro ao criar a thread");
+        exit(1);
+    }
+
+    pthread_join(thread3, NULL);
+    
+    sleep(1);
+    
+    printf("thread4 finalizada!\n");
+    
+    pthread_exit(NULL);
+}
+
+//THREAD 5
+void* t5(void* arg) {
+    
+    sleep(1);
+    
+    printf("thread5 iniciada!\n");
+    
+    pthread_t thread4;
+    
+    int res4 = pthread_create(&thread4, NULL, t4, NULL);
+
+    if (res4 != 0) {
+        perror("Erro ao criar a thread");
+        exit(1);
+    }
+
+    pthread_join(thread4, NULL);
+    
+    sleep(1);
+    
+    printf("thread5 finalizada!\n");
+    
+    pthread_exit(NULL);
+}
+
+//MAIN
+int main() {
+    pthread_t thread5;
+    
+    int res5 = pthread_create(&thread5, NULL, t5, NULL);
+
+    if (res5 != 0) {
+        perror("Erro ao criar a thread");
+        return 1;
+    }
+
+    pthread_join(thread5, NULL);
+    
+    sleep(1);
+
+    printf("todas as threads foram finalizadas!\n");
+    return 0;
+}
